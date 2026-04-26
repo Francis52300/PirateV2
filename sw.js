3d497cd76348a25885cfadb25371c7a53cd175b3
Francis52300 <f.edot@free.fr>
2026-04-26 11:50:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,9 +6,9 @@
    - APIs externes (fonts, Leaflet, React) : Stale While Revalidate
    ================================================================ */
 
-const CACHE_VERSION    = 'tresor-pirate-v11';
-const TILE_CACHE       = 'tresor-tiles-v11';
-const ASSETS_CACHE     = 'tresor-assets-v11';
+const CACHE_VERSION    = 'tresor-pirate-v12';
+const TILE_CACHE       = 'tresor-tiles-v12';
+const ASSETS_CACHE     = 'tresor-assets-v12';
 
 /* Assets à précacher au moment de l'installation */
 const PRECACHE_ASSETS = [
